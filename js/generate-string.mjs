2a1c92f072dc7ce01d21ee11c41683500a7e0732
Snap <XxSnapperGeexX@gmail.com>
2023-08-17 15:49:03 -0700

    Init generate-stirng module.

---
+++ js/generate-string.mjs
@@ -0,0 +1,55 @@
+const createCharArray = (start, end) =>
+{
+    const charArray = [];
+
+    for (let codePoint = start; codePoint <= end; ++codePoint) {
+        charArray.push(String.fromCharCode(codePoint));
+    }
+
+    return charArray;
+}
+
+const uppercaseChars = Object.freeze(createCharArray(65, 90));
+const lowercaseChars = Object.freeze(createCharArray(97, 122));
+const numericChars = Object.freeze(createCharArray(48, 57));
+const symbolChars = Object.freeze([
+    ...createCharArray(32, 47),
+    ...createCharArray(91, 96),
+    ...createCharArray(58, 64),
+    ...createCharArray(123, 126)
+]);
+
+export const chars = Object.freeze({
+    upperCase: uppercaseChars,
+    lowercase: lowercaseChars,
+    numeric: numericChars,
+    symbol: symbolChars
+});
+
+export const generateString = (length, chars) => {
+    if (typeof length !== "number")
+    {
+        throw new TypeError(`${generateString.name}: non number length argument.`);
+    }
+
+    if (length < 0)
+    {
+        throw new Error(`${generateString.name}: length less than 0.`);
+    }
+
+    let generatedString = "";
+
+    if ( ! (chars instanceof Array) || chars.some(element => typeof element !== "string"))
+    {
+        throw new TypeError(`${generateString.name}: non array of strings.`);
+    }
+
+    for (let charIndex = 0; charIndex < length; ++charIndex) {
+        const randomCharIndex = Math.floor(Math.random() * chars.length)
+        generatedString += chars[randomCharIndex];
+    }
+
+    return generatedString;
+}
+
+export default generateString;
