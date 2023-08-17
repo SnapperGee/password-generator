4be13d42a12f7326d842ed417725e49264a1425f
Snap <XxSnapperGeexX@gmail.com>
2023-08-16 23:33:37 -0700

    Add comments.

--- js/script.mjs
+++ js/script.mjs
@@ -17,6 +17,7 @@ const generateBtn = document.querySelector("#generate");
 
 window.onload = () =>
 {
-    const selectOptionRange = selectWithOptionNumberRange({name: "pwd-length", minRange: 8, maxRange: 128});
+    // NOTE!: Make sure name matches html label for attribute
+    const selectOptionRange = selectWithOptionNumberRange({name: "pwd-length-dropdown", minRange: 8, maxRange: 128});
     document.getElementById("pwd-length-dropdown-label")?.append(selectOptionRange);
 }
